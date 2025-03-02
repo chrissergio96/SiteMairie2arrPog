1bd89c23d1d3ceaf57a9466bdea13ab728ae2cf0
chrissergio96 <safou.christiansergio@gmail.com>
2025-03-02 20:16:07 +0100

    'mois'

--- src/ComponentsUrbanisme/RubriqueUrb/RubriqueUrb.jsx
+++ src/ComponentsUrbanisme/RubriqueUrb/RubriqueUrb.jsx
@@ -5,30 +5,30 @@ import { Link } from 'react-router-dom';
 
 const RubriqueUrb = () =>  {
     return (
-        <div className="rubrique">
+        <div className="rubriquet">
            <h2>Rubriques</h2>
 
-           <div>
+           <div className='four'>
             <Link to='/amenagements'>
-            <button className='travaux'>
+            <button className='travauxi'>
            <img src={log} alt="gg_arrow-up-o.png" />
            <h4>Aménagements</h4>
            </button>
            </Link>
            <Link to='/reglesUrb'>
-           <button className='travaux'>
+           <button className='travauxi'>
            <img src={log} alt="gg_arrow-up-o.png" />
            <h4>Règles d'urbanisme</h4>
            </button>
            </Link>
            <Link to='/autorisationsurbanisme'>
-           <button className='travaux'>
+           <button className='travauxi'>
            <img src={log} alt="gg_arrow-up-o.png" />
            <h4>Autorisations d'urbanisme</h4>
            </button>
            </Link>
            <Link to='/travauxurbanisme'>
-           <button className='travaux'>
+           <button className='travauxi'>
            <img src={log} alt="gg_arrow-up-o.png" />
            <h4>Travaux</h4>
            </button>
