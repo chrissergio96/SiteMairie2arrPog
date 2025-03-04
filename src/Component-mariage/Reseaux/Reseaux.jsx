4079680c9e7a98b4ad527f1bee3ff43b1b0347ee
chrissergio96 <safou.christiansergio@gmail.com>
2025-03-04 13:11:33 +0100

    'koj'

--- src/Component-mariage/Reseaux/Reseaux.jsx
+++ src/Component-mariage/Reseaux/Reseaux.jsx
@@ -7,10 +7,10 @@ function contactez() {
   return (
     <div className='reseaux'>
       <h5 className='nos-reseaux'>Nos reseaux:</h5>
-        <button type='buton' className='facebook'><h6>Facebook <a href="https://www.facebook.com/Mairie2eArrondissementPortGentil"><FacebookIcon className='icon-facebook1' color='primary' /></a></h6>
+        <button type='buton' className='facebook'><h6>Facebook :<a href="https://www.facebook.com/Mairie2eArrondissementPortGentil"><FacebookIcon className='icon-facebook1' color='primary' /></a></h6>
         </button>
 
-        <button type='button' className='whatsapp'><h6>whatsapp <a href="https://wa.me/24160371461"><WhatsAppIcon className='icon-facebook1' color='success' /></a></h6>
+        <button type='button' className='whatsapp'><h6>whatsapp :<a href="https://wa.me/24160371461"><WhatsAppIcon className='icon-facebook1' color='success' /></a></h6>
         </button>
         
     </div>
